0d49979b2a0b92f26b05392efaaa391c110f1854
John Welsh <jaybdub@users.noreply.github.com>
2023-09-10 14:45:05 -0700

    asdf

--- nanoowl/utils/module_recorder.py
+++ nanoowl/utils/module_recorder.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -8,26 +9,44 @@ class ModuleRecorder(object):
         self._input = None
         self._output = None
         self._hook = None
+        self._forward_pre_hook = None
+        self._forward_hook = None
+        self._start = torch.cuda.Event(enable_timing=True)
+        self._end = torch.cuda.Event(enable_timing=True)
 
+    def _on_pre_forward(self, module, input):
+        self._start.record()
+        
     def _on_forward(self, module, input, output):
         self._input = input
         self._output = output
+        self._end.record()
 
     def attach(self):
-        if self._hook is not None:
+        if self._forward_hook is not None:
             raise RuntimeError("Hook already attached.")
-        self._hook = self._module.register_forward_hook(self._on_forward)
+        if self._forward_pre_hook is not None:
+            raise RuntimeError("Hook already attached.")
+        self._forward_pre_hook = self._module.register_forward_pre_hook(self._on_pre_forward)
+        self._forward_hook = self._module.register_forward_hook(self._on_forward)
 
     def detach(self):
-        if self._hook is not None:
-            self._hook.remove()
-            self._hook = None
+        if self._forward_hook is not None:
+            self._forward_hook.remove()
+            self._forward_hook = None
+        if self._forward_pre_hook is not None:
+            self._forward_pre_hook.remove()
+            self._forward_pre_hook = None
 
     def __enter__(self, *args, **kwargs):
         self.attach()
 
     def __exit__(self, *args, **kwargs):
         self.detach()
+
+    def get_elapsed_time(self):
+        self._end.wait()
+        return self._start.elapsed_time(self._end)
 
     def get_input(self):
         return self._input
