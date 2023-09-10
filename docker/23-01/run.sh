de198aceddb3646c820889a812c4ae621bf3c77f
John Welsh <jaybdub@users.noreply.github.com>
2023-09-10 15:11:41 -0700

    docker

--- docker/23-01/run.sh
+++ docker/23-01/run.sh
@@ -8,7 +8,6 @@ docker run \
     --ipc host \
     --gpus all \
     --shm-size 14G \
-    --device /dev/video0:/dev/video0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -p 5000:5000 \
