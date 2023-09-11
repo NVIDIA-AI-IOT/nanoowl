eeb72a8c991ab6e83a3bfeab4bf9e147bb1eaf81
John Welsh <jaybdub@users.noreply.github.com>
2023-09-11 16:14:40 -0700

    profile owl components

--- nanoowl/tools/profile_components.py
+++ nanoowl/tools/profile_components.py
@@ -15,9 +15,9 @@ owlvit = OwlVit()
 
 image = PIL.Image.open("assets/dogs.jpg")
 
-vision_model_trt = load_image_encoder_engine("data/owlvit_vision_model.engine", owlvit.model.owlvit.vision_model.post_layernorm)
+# vision_model_trt = load_image_encoder_engine("data/owlvit_vision_model.engine", owlvit.model.owlvit.vision_model.post_layernorm)
 
-owlvit.model.owlvit.vision_model = vision_model_trt
+# owlvit.model.owlvit.vision_model = vision_model_trt
 
 vision_recorder = ModuleRecorder(owlvit.model.owlvit.vision_model)
 text_recorder = ModuleRecorder(owlvit.model.owlvit.text_model)
@@ -35,3 +35,17 @@ with vision_recorder, text_recorder, full_recorder:
 print(f"VISION MODEL: {vision_recorder.get_elapsed_time()}")
 print(f"TEXT MODEL: {text_recorder.get_elapsed_time()}")
 print(f"FULL MODEL: {full_recorder.get_elapsed_time()}")
+
+def to_ms(x):
+    return x / 1e9
+detections = owlvit.predict(image, texts=["a dog"])
+
+print(to_ms(owlvit.times['preprocess_text'] - owlvit.times['start']))
+print(to_ms(owlvit.times['preprocess_images'] - owlvit.times['preprocess_text']))
+print(to_ms(owlvit.times['move_inputs'] - owlvit.times['preprocess_images']))
+print(to_ms(owlvit.times['infer'] - owlvit.times['move_inputs']))
+print(to_ms(owlvit.times['move_outputs'] - owlvit.times['infer']))
+print(to_ms(owlvit.times['postprocess'] - owlvit.times['move_outputs']))
+print(to_ms(owlvit.times['end'] - owlvit.times['postprocess']))
+
+# print(owlvit.processor.image_processor)
--- nanoowl/utils/owlvit.py
+++ nanoowl/utils/owlvit.py
@@ -12,6 +12,7 @@ from transformers.models.owlvit.modeling_owlvit import (
 from typing import Sequence, List, Tuple
 import torch.nn as nn
 import time
+import numpy as np
 
 def remap_output(output, device):
     if isinstance(output, torch.Tensor):
@@ -25,27 +26,51 @@ def remap_output(output, device):
 
 class OwlVit(object):
     def __init__(self, threshold=0.1, device="cuda"):
-        self.processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32", device_map="cpu")
+        self.processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32", device_map=device)
         self.model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32", device_map=device)
         self.threshold = threshold
         self.device = device
         self.times = {}
+        self.inputs_images = {"pixel_values": torch.randn(1, 3, 768, 768)}
 
     def predict(self, image: PIL.Image.Image, texts: Sequence[str]):
+        torch.cuda.current_stream().synchronize()
         self.times = {}
         self.times['start'] = time.perf_counter_ns()
-        inputs = self.processor(text=texts, images=image, return_tensors="pt")
+        inputs_text = self.processor(text=texts, return_tensors="pt")
 
-        self.times['preprocess'] = time.perf_counter_ns()
+        torch.cuda.current_stream().synchronize()
+        self.times['preprocess_text'] = time.perf_counter_ns()
+        # inputs_images = self.processor(images=image, return_tensors="pt")
+        # print(inputs_images["pixel_values"].shape)
+        # inputs_images  = remap_output(self.inputs_images, "cuda")
+        image = np.asarray(image.resize((768,768)))
+        image = torch.from_numpy(image).permute(2, 0, 1).cuda()[None, ...].float()
+        inputs_images = {"pixel_values": image}#self.inputs_images#{"pixel_values": torch.randn(1, 3, 768, 768)}
+
+        torch.cuda.current_stream().synchronize()
+        self.times['preprocess_images'] = time.perf_counter_ns()
+        inputs = {}
+        inputs.update(inputs_text)
+        inputs.update(inputs_images)
 
         inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        torch.cuda.current_stream().synchronize()
+        self.times['move_inputs'] = time.perf_counter_ns()
+
         outputs = self.model(**inputs)
+        
+        torch.cuda.current_stream().synchronize()
+        self.times['infer'] = time.perf_counter_ns()
+
         outputs = remap_output(outputs, "cpu")
 
-        self.times['infer'] = time.perf_counter_ns()
+        torch.cuda.current_stream().synchronize()
+        self.times['move_outputs'] = time.perf_counter_ns()
 
         target_sizes = torch.Tensor([image.size[::-1]])
         results = self.processor.post_process_object_detection(outputs=outputs, target_sizes=target_sizes, threshold=self.threshold)
+        torch.cuda.current_stream().synchronize()
         self.times['postprocess'] = time.perf_counter_ns()
         i = 0
         boxes, scores, labels = results[i]["boxes"], results[i]["scores"], results[i]["labels"]
@@ -53,5 +78,6 @@ class OwlVit(object):
         for box, score, label in zip(boxes, scores, labels):
             detection = {"bbox": box.tolist(), "score": float(score), "label": int(label), "text": texts[label]}
             detections.append(detection)
+        torch.cuda.current_stream().synchronize()
         self.times['end'] = time.perf_counter_ns()
         return detections
