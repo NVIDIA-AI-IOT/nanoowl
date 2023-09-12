3594d7162cc2e88799c97e679e1730bb0e4b16f4
John Welsh <jaybdub@users.noreply.github.com>
2023-09-12 15:37:59 -0700

    basic usage vision model

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -8,12 +8,19 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", type=str, default="assets/owl_glove.jpg")
-    parser.add_argument("--vision_engine", type=str, default="data/owlvit_vision_model.engine")
+    parser.add_argument("--vision_engine", type=str, default=None)
+    parser.add_argument("--vision_model_name", type=str, default=None)
+    parser.add_argument("--vision_checkpoint", type=str, default=None)
     parser.add_argument("--prompt", action='append', required=True)
     parser.add_argument("--thresh", type=float, default=0.1)
     args = parser.parse_args()
     print(args)
-    predictor = Predictor(threshold=args.thresh, vision_engine=args.vision_engine)
+    predictor = Predictor(
+        threshold=args.thresh, 
+        vision_engine=args.vision_engine,
+        vision_model_name=args.vision_model_name,
+        vision_checkpoint=args.vision_checkpoint
+    )
 
     image = PIL.Image.open(args.image)
 
