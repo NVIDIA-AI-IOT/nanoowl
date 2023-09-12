9b650ff7bdb332148bb9d3ba2e3c6f3377dd02c5
John Welsh <jaybdub@users.noreply.github.com>
2023-09-12 00:24:49 -0700

    replace drawing

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", type=str, default="assets/owl_glove.jpg")
     parser.add_argument("--vision_engine", type=str, default="data/owlvit_vision_model.engine")
-    parser.add_argument("--prompt", nargs='+', required=True)
+    parser.add_argument("--prompt", action='append', required=True)
     parser.add_argument("--thresh", type=float, default=0.1)
     args = parser.parse_args()
-    
+    print(args)
     predictor = Predictor(threshold=args.thresh, vision_engine=args.vision_engine)
 
     image = PIL.Image.open(args.image)
