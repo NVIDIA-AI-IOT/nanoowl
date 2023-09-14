8efa532c5d719a361b109f9f73681a64036802f9
John Welsh <jaybdub@users.noreply.github.com>
2023-09-14 08:39:26 -0700

    rm model

--- nanoowl/utils/predictor.py
+++ nanoowl/utils/predictor.py
@@ -1,12 +1,13 @@
 
+from typing import Sequence
+
 import PIL.Image
 import torch
-from transformers import (
-    OwlViTProcessor
-)
-from typing import Sequence
+from transformers import OwlViTForObjectDetection, OwlViTProcessor
+
+from nanoowl.models import create_model
+from nanoowl.utils.tensorrt import load_image_encoder_engine
 from nanoowl.utils.transform import build_owlvit_vision_transform
-from nanoowl.utils.model import load_owlvit_model
 
 
 def remap_output(output, device):
@@ -18,6 +19,31 @@ def remap_output(output, device):
         for k, v in output.items():
             resdict[k] = remap_output(v,device)
         return res(**resdict)
+
+
+def load_owlvit_model(
+        device="cuda", 
+        vision_engine=None,
+        vision_checkpoint=None,
+        vision_model_name=None
+    ):
+    
+    model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32", device_map=device)
+
+    # Overwrite with different vision encoder
+    if vision_model_name is not None:
+        assert vision_checkpoint is not None
+        vision_model = create_model(vision_model_name)
+        vision_model.load_state_dict(torch.load(vision_checkpoint)['model'])
+        vision_model = vision_model.eval().to(device)
+        model.owlvit.vision_model = vision_model
+
+    # Overwrite with engine
+    if vision_engine is not None:
+        vision_model_trt = load_image_encoder_engine(vision_engine, model.owlvit.vision_model.post_layernorm)
+        model.owlvit.vision_model = vision_model_trt
+    
+    return model
 
 
 class Predictor(object):
