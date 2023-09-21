6385ac925dcf84f54a2fbdf91b8a7a719ca38dce
John Welsh <jaybdub@users.noreply.github.com>
2023-09-21 14:02:18 -0700

    profile

--- nanoowl/profile.py
+++ nanoowl/profile.py
@@ -1,37 +1,46 @@
-import pytest
+import argparse
 import PIL.Image
 from nanoowl.model import (
     OwlVitPredictor,
-    OwlVitImagePreprocessorModule,
-    OwlVitImageEncoderModule,
     capture_timings
 )
 from nanoowl.utils.drawing import draw_detections_raw
 
 
 if __name__ == "__main__":
-        
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", type=str, default="assets/owl_glove.jpg")
+    parser.add_argument("--text", action='append', required=True)
+    parser.add_argument("--threshold", type=float, default=0.1)
+    parser.add_argument("--output", type=str, default="data/owl_glove_out.jpg")
+    parser.add_argument("--model", type=str, default="google/owlvit-base-patch32")
+    parser.add_argument("--image_encoder_engine", type=str, default="data/owlvit-base-patch32-image-encoder.engine")
+    parser.add_argument("--text_encoder_engine", type=str, default="data/owlvit-base-patch32-text-encoder.engine")
+    parser.add_argument("--output_path", type=str, default="data/predict_out.jpg")
+    args = parser.parse_args()
+
+    if args.image_encoder_engine.lower() == "none":
+        args.image_encoder_engine = None
+
+    if args.text_encoder_engine.lower() == "none":
+        args.text_encoder_engine = None
+
     predictor = OwlVitPredictor.from_pretrained(
-        "google/owlvit-base-patch32",
-        image_encoder_engine="data/owlvit-base-patch32-image-encoder.engine",
-        text_encoder_engine="data/owlvit-base-patch32-text-encoder.engine"
+        args.model,
+        image_encoder_engine=args.image_encoder_engine,
+        text_encoder_engine=args.text_encoder_engine,
+        device="cuda"
     )
 
-    image = PIL.Image.open("assets/camera.jpg")
+    image = PIL.Image.open(args.image)
 
-    detections = predictor.predict(image=image, text=["an owl", "a glove"])
-    
+    text = args.text
+
+    detections = predictor.predict(image=image, text=text, threshold=args.threshold)
 
     with capture_timings() as timings:
         for i in range(20):
-            detections = predictor.predict(image=image, text=["an owl", "a glove"])
+            detections = predictor.predict(image=image, text=args.text)
     
     timings.print_median_elapsed_times_ms()
-
-    # draw_detections_raw(image, detections)
-    # # print(len(detections))
-    # # for detection in detections:
-    # #     draw_detection(image, detection)
-
-    # image.save("data/test_owlvit_predictor_cuda.jpg")
-
