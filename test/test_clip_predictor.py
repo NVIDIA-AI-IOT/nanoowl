632979c51e159b047cfb50773c2baf4ef10ae1af
John Welsh <jwelsh@nvidia.com>
2023-10-06 12:55:07 -0700

    test clip

--- test/test_clip_predictor.py
+++ test/test_clip_predictor.py
@@ -2,23 +2,12 @@ import pytest
 import torch
 import PIL.Image
 from nanoowl.clip_predictor import ClipPredictor
+from nanoowl.image_preprocessor import ImagePreprocessor
 
 
 def test_get_image_size():
     clip_predictor = ClipPredictor()
     assert clip_predictor.get_image_size() == (224, 224)
-
-
-def test_clip_preprocess_pil_image():
-
-    clip_predictor = ClipPredictor()
-
-    image = PIL.Image.open("assets/owl_glove_small.jpg")
-
-    image_tensor = clip_predictor.preprocess_pil_image(image)
-
-    assert image_tensor.shape == (1, 3, 499, 756)
-    assert torch.allclose(image_tensor.mean(), torch.zeros_like(image_tensor), atol=1, rtol=1)
 
 
 def test_clip_encode_text():
@@ -38,7 +27,9 @@ def test_clip_encode_image():
 
     image = image.resize((224, 224))
 
-    image_tensor = clip_predictor.preprocess_pil_image(image)
+    image_preprocessor = ImagePreprocessor().to(clip_predictor.device).eval()
+
+    image_tensor = image_preprocessor.preprocess_pil_image(image)
 
     image_encode_output = clip_predictor.encode_image(image_tensor)
 
@@ -52,7 +43,9 @@ def test_clip_classify():
     image = PIL.Image.open("assets/frog.jpg")
 
     image = image.resize((224, 224))
-    image_tensor = clip_predictor.preprocess_pil_image(image)
+    image_preprocessor = ImagePreprocessor().to(clip_predictor.device).eval()
+
+    image_tensor = image_preprocessor.preprocess_pil_image(image)
 
     text_output = clip_predictor.encode_text(["a frog", "an owl"])
     image_output = clip_predictor.encode_image(image_tensor)
