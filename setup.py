967fbd392a1ddb48a7d789200511d1910bfd7743
manuel cuevas <manuel@thinkdeeply.com>
2024-07-30 12:04:22 -0700

    setup version update

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="nanoowl",
-    version="0.0.1",
+    version="0.0.2",
+    description='NanoOWL is a project that optimizes OWL-ViT to run '
+                '🔥 real-time 🔥 on NVIDIA Jetson Orin Platforms with '
+                'NVIDIA TensorRT',
     packages=find_packages()
 )
