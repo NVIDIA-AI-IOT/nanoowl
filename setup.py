d3071dcf09996bec18d8f30d7cd1bbfa3ab1be20
Aaron <aaronrmm@gmail.com>
2024-11-12 16:13:33 -0500

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="nanoowl",
-    version="0.0.4.dev0+73aacfe",
+    version="0.1.0",
     description="NanoOWL is a project that optimizes OWL-ViT to run "
     "🔥 real-time 🔥 on NVIDIA Jetson Orin Platforms with "
     "NVIDIA TensorRT",
